6e4f9ba0a8cddec6ea3f627a6251378bf055e21d
itsybitsyspyda <kimathicodes@gmail.com>
2026-05-18 18:08:27 +0300

    Spotify stats

--- server/models/FriendRequest.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const friendRequestSchema = new mongoose.Schema ({
-    from: {
-        type: mongoose.Schema.Type.ObjectId,
-        ref: 'User',
-        required: true
-    },
-});
